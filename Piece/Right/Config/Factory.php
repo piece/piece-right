6cbc598e5398fda3d6b9a225611356935c40d4d3
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-18 05:37:31 +0000

    - Added.

---
+++ Piece/Right/Config/Factory.php
@@ -0,0 +1,275 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      File available since Release 0.1.0
+ */
+
+require_once 'Piece/Right/Config.php';
+require_once 'Piece/Right/Error.php';
+require_once 'Cache/Lite/File.php';
+require_once 'PEAR.php';
+
+if (version_compare(phpversion(), '5.0.0', '<')) {
+    require_once 'spyc.php';
+} else {
+    require_once 'spyc.php5';
+}
+
+// {{{ Piece_Right_Config_Factory
+
+/**
+ * A factory class for creating Piece_Right_Config objects.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      Class available since Release 0.1.0
+ */
+class Piece_Right_Config_Factory
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ factory()
+
+    /**
+     * Creates a Piece_Right_Config object from a configuration file or a
+     * cache.
+     *
+     * @param string $name
+     * @param string $configDirectory
+     * @param string $cacheDirectory
+     * @return Piece_Right_Config
+     * @static
+     */
+    function &factory($name = null, $configDirectory = null, $cacheDirectory = null)
+    {
+        if (is_null($name) || is_null($configDirectory)) {
+            $config = &new Piece_Right_Config();
+            return $config;
+        }
+
+        if (!file_exists($configDirectory)) {
+            Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Right_Error::push(PIECE_RIGHT_ERROR_NOT_FOUND,
+                                    "The configuration directory [ $configDirectory ] not found.",
+                                    'warning'
+                                    );
+            Piece_Right_Error::popCallback();
+            $config = &new Piece_Right_Config();
+            return $config;
+        }
+
+        $configFile = "$configDirectory/$name.yaml";
+
+        if (!file_exists($configFile)) {
+            Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Right_Error::push(PIECE_RIGHT_ERROR_NOT_FOUND,
+                                    "The configuration file [ $configFile ] not found.",
+                                    'warning'
+                                    );
+            Piece_Right_Error::popCallback();
+            $config = &new Piece_Right_Config();
+            return $config;
+        }
+
+        if (!is_readable($configFile)) {
+            Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Right_Error::push(PIECE_RIGHT_ERROR_NOT_READABLE,
+                                    "The configuration file [ $configFile ] was not readable.",
+                                    'warning'
+                                    );
+            Piece_Right_Error::popCallback();
+            $config = &new Piece_Right_Config();
+            return $config;
+        }
+
+        if (is_null($cacheDirectory)) {
+            $cacheDirectory = './cache';
+        }
+
+        if (!file_exists($cacheDirectory)) {
+            Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Right_Error::push(PIECE_RIGHT_ERROR_NOT_FOUND,
+                                    "The cache directory [ $cacheDirectory ] not found.",
+                                    'warning'
+                                    );
+            Piece_Right_Error::popCallback();
+            $config = &Piece_Right_Config_Factory::_parseFile($configFile);
+            return $config;
+        }
+
+        if (!is_readable($cacheDirectory)
+            || !is_writable($cacheDirectory)
+            ) {
+            Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Right_Error::push(PIECE_RIGHT_ERROR_NOT_READABLE,
+                                    "The cache directory [ $cacheDirectory ] was not readable or writable.",
+                                    'warning'
+                                    );
+            Piece_Right_Error::popCallback();
+            $config = &Piece_Right_Config_Factory::_parseFile($configFile);
+            return $config;
+        }
+
+        $config = &Piece_Right_Config_Factory::_getConfiguration($cacheDirectory,
+                                                                 $configFile
+                                                                 );
+
+        return $config;
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    // }}}
+    // {{{ _getConfiguration()
+
+    /**
+     * Gets a Piece_Right_Config object from a configuration file or a cache.
+     *
+     * @param string $cacheDirectory
+     * @param string $masterFile
+     * @return Piece_Right_Config
+     * @static
+     */
+    function &_getConfiguration($cacheDirectory, $masterFile)
+    {
+        $cache = &new Cache_Lite_File(array('cacheDir' => "$cacheDirectory/",
+                                            'masterFile' => $masterFile,
+                                            'automaticSerialization' => true,
+                                            'errorHandlingAPIBreak' => true)
+                                      );
+
+        /*
+         * The Cache_Lite class always specifies PEAR_ERROR_RETURN when
+         * calling PEAR::raiseError in default.
+         */
+        $config = $cache->get($masterFile);
+        if (PEAR::isError($config)) {
+            Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Right_Error::push(PIECE_RIGHT_ERROR_CANNOT_READ,
+                                    "Cannot read the cache file in the directory [ $cacheDirectory ].",
+                                    'warning'
+                                    );
+            Piece_Right_Error::popCallback();
+            $config = &Piece_Right_Config_Factory::_parseFile($masterFile);
+            return $config;
+        }
+
+        if (!$config) {
+            $config = &Piece_Right_Config_Factory::_parseFile($masterFile);
+            $result = $cache->save($config);
+            if (PEAR::isError($result)) {
+                Piece_Right_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+                Piece_Right_Error::push(PIECE_RIGHT_ERROR_CANNOT_WRITE,
+                                        "Cannot write the Piece_Right_Config object to the cache file in the directory [ $cacheDirectory ].",
+                                        'warning'
+                                        );
+                Piece_Right_Error::popCallback();
+            }
+        }
+
+        return $config;
+    }
+
+    // }}}
+    // {{{ _parseFile()
+
+    /**
+     * Parses the given file and returns a Piece_Right_Config object.
+     *
+     * @param string $file
+     * @return Piece_Right_Config
+     * @static
+     */
+    function &_parseFile($file)
+    {
+        $config = &new Piece_Right_Config();
+        $yaml = Spyc::YAMLLoad($file);
+        foreach ($yaml as $validation) {
+            foreach ($validation['validator'] as $validator) {
+                $config->addValidation($validation['name'],
+                                       $validator['name'],
+                                       (array)@$validator['rules']
+                                       );
+            }
+        }
+
+        return $config;
+    }
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
---
+++ Piece/Right/Error.php
@@ -0,0 +1,213 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      File available since Release 0.1.0
+ */
+
+require_once 'PEAR/ErrorStack.php';
+
+// {{{ constants
+
+/*
+ * Error codes
+ */
+define('PIECE_RIGHT_ERROR_NOT_FOUND',    -1);
+define('PIECE_RIGHT_ERROR_NOT_READABLE', -2);
+define('PIECE_RIGHT_ERROR_CANNOT_READ',  -3);
+define('PIECE_RIGHT_ERROR_CANNOT_WRITE', -4);
+
+// }}}
+// {{{ Piece_Right_Error
+
+/**
+ * An error class for Piece_Right package.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      Class available since Release 0.1.0
+ */
+class Piece_Right_Error
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     * @static
+     */
+
+    // }}}
+    // {{{ push()
+
+    /**
+     * Adds an error to the stack for the package. This method is a wrapper
+     * for PEAR_ErrorStack::staticPush() method.
+     *
+     * @param integer $code
+     * @param string  $message
+     * @param string  $level
+     * @param array   $params
+     * @param array   $repackage
+     * @param array   $backtrace
+     * @see PEAR_ErrorStack::staticPush()
+     */
+    function push($code, $message = false, $level = 'exception',
+                  $params = array(), $repackage = false, $backtrace = false
+                  )
+    {
+        if (!$backtrace) {
+            $backtrace = debug_backtrace();
+        }
+
+        PEAR_ErrorStack::staticPush('Piece_Right', $code, $level, $params, $message, $repackage, $backtrace);
+    }
+
+    // }}}
+    // {{{ pushCallback()
+
+    /**
+     * Pushes a callback. This method is a wrapper for
+     * PEAR_ErrorStack::staticPushCallback() method.
+     *
+     * @param callback $callback
+     * @see PEAR_ErrorStack::staticPushCallback()
+     */
+    function pushCallback($callback)
+    {
+        PEAR_ErrorStack::staticPushCallback($callback);
+    }
+
+    // }}}
+    // {{{ popCallback()
+
+    /**
+     * Pops a callback. This method is a wrapper for
+     * PEAR_ErrorStack::staticPopCallback() method.
+     *
+     * @return callback
+     * @see PEAR_ErrorStack::staticPopCallback()
+     */
+    function popCallback()
+    {
+        return PEAR_ErrorStack::staticPopCallback();
+    }
+
+    // }}}
+    // {{{ hasErrors()
+
+    /**
+     * Returns whether the stack has errors or not. This method is a wrapper
+     * for PEAR_ErrorStack::staticHasErrors() method.
+     *
+     * @param string $level
+     * @return boolean
+     * @see PEAR_ErrorStack::staticHasErrors()
+     */
+    function hasErrors($level = false)
+    {
+        return PEAR_ErrorStack::staticHasErrors('Piece_Right', $level);
+    }
+
+    // }}}
+    // {{{ pop()
+
+    /**
+     * Pops an error off of the error stack for the package. This method is a
+     * wrapper for PEAR_ErrorStack::pop() method.
+     *
+     * @return array
+     * @see PEAR_ErrorStack::pop()
+     */
+    function pop()
+    {
+        $stack = &PEAR_ErrorStack::singleton('Piece_Right');
+        return $stack->pop();
+    }
+
+    // }}}
+    // {{{ clearErrors()
+
+    /**
+     * Clears the error stack for the package.
+     *
+     * @see PEAR_ErrorStack::getErrors()
+     */
+    function clearErrors()
+    {
+        $stack = &PEAR_ErrorStack::singleton('Piece_Right');
+        $stack->getErrors(true);
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
