c2b90b0992f33820fcde5527f9eacc48fd7ff7fd
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-26 02:17:18 +0000

    - Added.

---
+++ Piece/Right/Validator/Date.php
@@ -0,0 +1,155 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      File available since Release 0.3.0
+ */
+
+require_once 'Piece/Right/Validator/Common.php';
+
+// {{{ Piece_Right_Validator_Date
+
+/**
+ * A validator which is used to check whether a value is a valid date.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      Class available since Release 0.3.0
+ */
+class Piece_Right_Validator_Date extends Piece_Right_Validator_Common
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    var $_year;
+    var $_month;
+    var $_day;
+    var $_pattern = '/^(\d+)-(\d+)-(\d+)$/';
+    var $_patternYearPosition = 1;
+    var $_patternMonthPosition = 2;
+    var $_patternDayPosition = 3;
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ validate()
+
+    /**
+     * Checks whether a value is a valid date.
+     *
+     * @param string $value
+     * @return boolean
+     */
+    function validate($value)
+    {
+        if (!is_null($this->_year)
+            && !is_null($this->_month)
+            && !is_null($this->_day)
+            ) {
+            return checkdate($this->_results->getFieldValue($this->_month),
+                             $this->_results->getFieldValue($this->_day),
+                             $this->_results->getFieldValue($this->_year)
+                             );
+        }
+
+        if (!preg_match($this->_pattern, $value, $matches)) {
+            return false;
+        }
+
+        return checkdate($matches[$this->_patternMonthPosition],
+                         $matches[$this->_patternDayPosition],
+                         $matches[$this->_patternYearPosition]
+                         );
+    }
+
+    // }}}
+    // {{{ clear()
+
+    /**
+     * Clears the values of all properties.
+     */
+    function clear()
+    {
+        $this->_year = null;
+        $this->_month = null;
+        $this->_day = null;
+        $this->_pattern = '/^(\d+)-(\d+)-(\d+)$/';
+        $this->_patternYearPosition = 1;
+        $this->_patternMonthPosition = 2;
+        $this->_patternDayPosition = 3;
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+ 
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
