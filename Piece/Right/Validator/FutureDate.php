7b753dacc698abab0f5d6799b9679d64927027cb
KUBO Atsuhiro <kubo@iteman.jp>
2006-08-09 14:46:04 +0000

    - Added.

---
+++ Piece/Right/Validator/FutureDate.php
@@ -0,0 +1,138 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      File available since Release 1.0.0
+ */
+
+require_once 'Piece/Right/Validator/Date.php';
+
+// {{{ Piece_Right_Validator_Date
+
+/**
+ * A validator which is used to check whether a value is a valid date and the
+ * value is later than current date.
+ *
+ * @package    Piece_Right
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      Class available since Release 1.0.0
+ */
+class Piece_Right_Validator_FutureDate extends Piece_Right_Validator_Date
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ validate()
+
+    /**
+     * Checks whether a value is a valid date and the value is later than
+     * current date.
+     *
+     * @param string $value
+     * @return boolean
+     */
+    function validate($value)
+    {
+        if (!parent::validate($value)) {
+            return false;
+        }
+
+        $allowCurrentDate = $this->getRule('allowCurrentDate');
+
+        $currentTime = time();
+        if (!$allowCurrentDate) {
+            return mktime(0, 0, 0, $this->_month, $this->_day, $this->_year) > mktime(0, 0, 0, date('n', $currentTime), date('j', $currentTime), date('Y', $currentTime));
+        } else {
+            return mktime(0, 0, 0, $this->_month, $this->_day, $this->_year) >= mktime(0, 0, 0, date('n', $currentTime), date('j', $currentTime), date('Y', $currentTime));
+        }
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    // }}}
+    // {{{ _initialize()
+
+    /**
+     * Initializes properties.
+     */
+    function _initialize()
+    {
+        parent::_initialize();
+        $this->_addRule('allowCurrentDate', false);
+    }
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
