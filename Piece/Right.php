19b32c93143ca50d3999848adcff9b7e7d5697c7
KUBO Atsuhiro <kubo@iteman.jp>
2006-08-25 07:37:54 +0000

    - Changed the code so as to not set payloads to any filters.

--- Piece/Right.php
+++ Piece/Right.php
@@ -293,7 +293,7 @@ class Piece_Right
                         return;
                     }
 
-                    $fieldValue = $filter->filter($fieldValue, $this->_payload);
+                    $fieldValue = $filter->filter($fieldValue);
                 } else {
                     $fieldValue = call_user_func($filterName, $fieldValue);
                 }
